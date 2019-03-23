bee5f9aec9271f3623c7b936d3a292b3b06c603a
CenIII <cenchuan2013@gmail.com>
2019-03-23 17:53:32 -0400

    ...

--- models/gcn.py
+++ models/gcn.py
@@ -172,9 +172,11 @@ class GCN(nn.Module):
         edges = np.array(edges)
         adj = sp.coo_matrix((np.ones(len(edges)), (edges[:, 0], edges[:, 1])),
                             shape=(n, n), dtype='float32')
-        self.raw_adj = adj#spm_to_tensor(adj).cuda()
+        raw_adj = normt_spm(adj, method='sym')
+        raw_adj = spm_to_tensor(raw_adj)
+        self.raw_adj = raw_adj.cuda() #spm_to_tensor(adj).cuda()
         norm_adj = normt_spm(adj, method=norm_method)
-        norm_adj = spm_to_tensor(adj)
+        norm_adj = spm_to_tensor(norm_adj)
         self.adj = norm_adj.cuda()
 
         hl = hidden_layers.split(',')
