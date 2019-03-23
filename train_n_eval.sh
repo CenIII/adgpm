8e37cfb84ee30b6897e4c50b7200114404eac998
CenIII <cenchuan2013@gmail.com>
2019-03-22 22:31:01 -0400

    ...

--- train_n_eval.sh
+++ train_n_eval.sh
@@ -6,49 +6,56 @@
 num_list=(0 1 2 3 4)
 ## shallow
 # original
-git checkout master
-save_prefix='./save/original'
-for i in "${num_list[@]}"; do
-	mkdir $save_prefix$i
-	python train_gcn_basic.py --save-path $save_prefix$i --max-epoch 5000 --save-epoch 3000 > $save_prefix$i'/train_log'
-done
+# git checkout master
+# save_prefix='./save/original'
+# for i in "${num_list[@]}"; do
+# 	mkdir $save_prefix$i
+# 	python train_gcn_basic.py --save-path $save_prefix$i --max-epoch 5000 --save-epoch 3000 > $save_prefix$i'/train_log'
+# done
 
-# sym
-save_prefix='./save/sym'
-for i in "${num_list[@]}"; do
-	mkdir $save_prefix$i
-	python train_gcn_basic.py --save-path $save_prefix$i --max-epoch 5000 --save-epoch 3000 --norm-method sym > $save_prefix$i'/train_log'
-done
+# # sym
+# save_prefix='./save/sym'
+# for i in "${num_list[@]}"; do
+# 	mkdir $save_prefix$i
+# 	python train_gcn_basic.py --save-path $save_prefix$i --max-epoch 5000 --save-epoch 3000 --norm-method sym > $save_prefix$i'/train_log'
+# done
 
 # random_fix
-git checkout random_fix
 save_prefix='./save/random_fix'
 for i in "${num_list[@]}"; do
+	git reset --hard HEAD
+	git checkout random_fix
 	mkdir $save_prefix$i
 	python train_gcn_basic.py --save-path $save_prefix$i --max-epoch 5000 --save-epoch 3000 > $save_prefix$i'/train_log'
 done
 
 # random_unfix
-git checkout random_unfix
+
 save_prefix='./save/random_unfix'
 for i in "${num_list[@]}"; do
+	git reset --hard HEAD
+	git checkout random_unfix
 	mkdir $save_prefix$i
 	python train_gcn_basic.py --save-path $save_prefix$i --max-epoch 5000 --save-epoch 3000 > $save_prefix$i'/train_log'
 done
 
 # EMFSaA
-git checkout EMFSaA
+
 save_prefix='./save/EMFSaA'
 for i in "${num_list[@]}"; do
+	git reset --hard HEAD
+	git checkout EMFSaA
 	mkdir $save_prefix$i
 	python train_gcn_basic.py --save-path $save_prefix$i --max-epoch 5000 --save-epoch 3000 > $save_prefix$i'/train_log'
 done
 
 ## Deep
 # original
-git checkout master
+
 save_prefix='./save/original_deep'
 for i in "${num_list[@]}"; do
+	git reset --hard HEAD
+	git checkout master
 	mkdir $save_prefix$i
 	python train_gcn_basic.py --save-path $save_prefix$i --max-epoch 5000 --save-epoch 3000 --layers 2048,2048,1024,1024,d512,d > $save_prefix$i'/train_log'
 done
@@ -56,30 +63,38 @@ done
 # sym
 save_prefix='./save/sym_deep'
 for i in "${num_list[@]}"; do
+	git reset --hard HEAD
+	git checkout master
 	mkdir $save_prefix$i
 	python train_gcn_basic.py --save-path $save_prefix$i --max-epoch 5000 --save-epoch 3000 --layers 2048,2048,1024,1024,d512,d > $save_prefix$i'/train_log'
 done
 
 # random_fix
-git checkout random_fix
+
 save_prefix='./save/random_fix_deep'
 for i in "${num_list[@]}"; do
+	git reset --hard HEAD
+	git checkout random_fix
 	mkdir $save_prefix$i
 	python train_gcn_basic.py --save-path $save_prefix$i --max-epoch 5000 --save-epoch 3000 --layers 2048,2048,1024,1024,d512,d > $save_prefix$i'/train_log'
 done
 
 # random_unfix
-git checkout random_unfix
+
 save_prefix='./save/random_unfix_deep'
 for i in "${num_list[@]}"; do
+	git reset --hard HEAD
+	git checkout random_unfix
 	mkdir $save_prefix$i
 	python train_gcn_basic.py --save-path $save_prefix$i --max-epoch 5000 --save-epoch 3000 --layers 2048,2048,1024,1024,d512,d > $save_prefix$i'/train_log'
 done
 
 # EMFSaA
-git checkout EMFSaA
+
 save_prefix='./save/EMFSaA_deep'
 for i in "${num_list[@]}"; do
+	git reset --hard HEAD
+	git checkout EMFSaA
 	mkdir $save_prefix$i
 	python train_gcn_basic.py --save-path $save_prefix$i --max-epoch 5000 --save-epoch 3000 --layers 2048,2048,1024,1024,d512,d > $save_prefix$i'/train_log'
 done
