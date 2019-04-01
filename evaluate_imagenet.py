9f7bd8094647f4c890922fb71d27986c9f8157b4
CenIII <cenchuan2013@gmail.com>
2019-04-01 09:28:54 -0400

    ...

--- evaluate_imagenet.py
+++ evaluate_imagenet.py
@@ -76,7 +76,9 @@ if __name__ == '__main__':
     pred_wnids = pred_file['wnids']
     pred_vectors = pred_file['pred']
     pred_dic = dict(zip(pred_wnids, pred_vectors))
-    pred_vectors = pick_vectors(pred_dic, train_wnids + test_wnids, is_tensor=True).cuda()
+    pred_vectors = pick_vectors(pred_dic, pred_wnids, is_tensor=True).cuda()
+
+    print('num pred_vectors: '+str(len(pred_vectors)))
 
     pred_vectors = pred_vectors.cuda()
 
