5c0eae20440abbf498cc9373dff177122a06817c
CenIII <cenchuan2013@gmail.com>
2019-03-31 23:57:11 -0400

    ...

--- evaluate_imagenet.py
+++ evaluate_imagenet.py
@@ -11,7 +11,7 @@ from datasets.imagenet import ImageNetFeats
 from utils import set_gpu, pick_vectors
 
 
-def test_on_subset(dataset, cnn, n, pred_vectors, all_label,
+def test_on_subset(dataset, cnn, n, fc_vectors, pred_vectors, all_label,
                    consider_trains):
     top = [1, 2, 5, 10, 20]
     hits = torch.zeros(len(top)).cuda()
@@ -24,12 +24,22 @@ def test_on_subset(dataset, cnn, n, pred_vectors, all_label,
         # data, label = batch 
     data = dataset.cuda()
 
-    feat = data #cnn(data) # (batch_size, d)
+    feat = data #cnn(data) # (batch_size, d)    # [113, 2049]
     # feat = torch.cat([feat, torch.ones(len(feat)).view(-1, 1).cuda()], dim=1)
 
-    fcs = pred_vectors.t()
+    embs = pred_vectors.t() #[512, 2549]
 
-    table = torch.matmul(feat, fcs)
+    # fc_vectors [2049, 1000]
+
+    conseAtt = torch.matmul(feat, fc_vectors)  # 113x1000
+
+    # softmax
+    coonseAtt = F.softmax(conseAtt,1)
+
+    # conseAtt x embs
+    preds = torch.matmul(conseAtt,embs[:,:1000].t()) # 113x512  every image get a embs estimation
+    # find nearest emb
+    table = torch.matmul(F.normalize(preds,1), F.normalize(embs))      # 113x2549
     if not consider_trains:
         table[:, :n] = -1e18
 
@@ -80,6 +90,18 @@ if __name__ == '__main__':
 
     pred_vectors = pred_vectors.cuda()
 
+    ### load gt weights
+    graph = json.load(open('materials/imagenet-induced-graph.json', 'r'))
+    wnids = graph['wnids']
+    n = len(wnids)
+    edges = graph['edges']
+    fcfile = json.load(open('materials/fc-weights.json', 'r'))
+    # train_wnids = [x[0] for x in fcfile]
+    fc_vectors = [x[1] for x in fcfile]
+    assert train_wnids == wnids[:len(train_wnids)]
+    fc_vectors = torch.tensor(fc_vectors).cuda()
+    fc_vectors = F.normalize(fc_vectors)
+
     n = len(train_wnids)
     m = len(test_wnids)
     
@@ -102,7 +124,7 @@ if __name__ == '__main__':
     if TEST_TRAIN:
         for i, wnid in enumerate(train_wnids, 1):
             subset = dataset.get_subset(wnid)  # return truncated matrix
-            hits, tot = test_on_subset(subset, cnn, n, pred_vectors, i - 1,
+            hits, tot = test_on_subset(subset, cnn, n, fc_vectors, pred_vectors, i - 1,
                                        consider_trains=args.consider_trains)
             results[wnid] = (hits / tot).tolist()
 
@@ -118,7 +140,7 @@ if __name__ == '__main__':
         count = 0
         for i, wnid in enumerate(test_wnids, 1):
             subset = dataset.get_subset(wnid)
-            hits, tot = test_on_subset(subset, cnn, n, pred_vectors, n + i - 1,
+            hits, tot = test_on_subset(subset, cnn, n, fc_vectors, pred_vectors, n + i - 1,
                                        consider_trains=args.consider_trains)
             results[wnid] = (hits / tot).tolist()
 
