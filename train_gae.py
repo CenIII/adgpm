73e7a8acd8ed1f7e9dd40d0d875b01c91e7139f6
CenIII <cenchuan2013@gmail.com>
2019-04-04 10:32:17 -0400

    ...

--- train_gae.py
+++ train_gae.py
@@ -81,7 +81,7 @@ if __name__ == '__main__':
     fc_vectors = F.normalize(fc_vectors)
 
     hidden_layers = args.layers #'d2048,d' #'2048,2048,1024,1024,d512,d'
-    gae = GAE(n, edges, word_vectors.shape[1], fc_vectors.shape[1], hidden_layers, inds2hops, args.norm_method).cuda()
+    gae = GAE(n, edges, word_vectors.shape[1], 512, hidden_layers, inds2hops, args.norm_method).cuda()  #fc_vectors.shape[1]
     crit = GAECrit(gae.pos_weight, gae.norm).cuda()
     targets = gae.getTargets().cuda()
     print('{} nodes, {} edges'.format(n, len(edges)))
